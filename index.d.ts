ef099991caca80446d6519631a8a6ce17a251c8f
Kevin <kevinleyssens@hotmail.com>
2020-01-14 14:12:12 +0100

    updated namespace

--- index.d.ts
+++ index.d.ts
@@ -9,29 +9,28 @@ declare module 'fabric-shim-crypto' {
     export = ShimCrypto;
 }
 
-/*~ Write your module's methods and properties in this class */
-declare class ShimCrypto {
-    constructor(stub: Object);
-
-    ENCRYPT_KEY: string;
-    SIGN_KEY: string;
-    INIT_VECTOR: string;
-
-
-    encrypt(plaintext: string): Buffer;
-
-    decrypt(ciphertext: string): Buffer;
-
-    sign(message: string): Buffer;
-
-    verify(signature: Buffer, message: string): ShimCrypto.VerifyResponse;
-
-}
-
 /*~ If you want to expose types from your module as well, you can
  *~ place them in this block.
  */
 declare namespace ShimCrypto {
+    /*~ Write your module's methods and properties in this class */
+    export class ShimCrypto {
+        constructor(stub: Object);
+
+        ENCRYPT_KEY: string;
+        SIGN_KEY: string;
+        INIT_VECTOR: string;
+
+        encrypt(plaintext: string): Buffer;
+
+        decrypt(ciphertext: string): Buffer;
+
+        sign(message: string): Buffer;
+
+        verify(signature: Buffer, message: string): ShimCrypto.VerifyResponse;
+
+    }
+
     export interface VerifyResponse {
         ok: boolean;
         error?: Error;
