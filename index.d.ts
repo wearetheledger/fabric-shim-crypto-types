ccd690f7645504da7829d44e773f1f10daa9d3a1
Kevin <kevinleyssens@hotmail.com>
2020-01-14 14:23:55 +0100

    change declaration

--- index.d.ts
+++ index.d.ts
@@ -6,33 +6,37 @@
 
 /* tslint:disable */
 declare module 'fabric-shim-crypto' {
+
+    namespace ShimCrypto {
+        /*~ Write your module's methods and properties in this class */
+
+        /*~ If you want to expose types from your module as well, you can
+        *~ place them in this block.
+        */
+        export class ShimCrypto {
+            constructor(stub: Object);
+
+            ENCRYPT_KEY: string;
+            SIGN_KEY: string;
+            INIT_VECTOR: string;
+
+            encrypt(plaintext: string): Buffer;
+
+            decrypt(ciphertext: string): Buffer;
+
+            sign(message: string): Buffer;
+
+            verify(signature: Buffer, message: string): ShimCrypto.VerifyResponse;
+
+        }
+
+        export interface VerifyResponse {
+            ok: boolean;
+            error?: Error;
+        }
+    }
+
     export = ShimCrypto;
 }
 
-/*~ If you want to expose types from your module as well, you can
- *~ place them in this block.
- */
-declare namespace ShimCrypto {
-    /*~ Write your module's methods and properties in this class */
-    export class ShimCrypto {
-        constructor(stub: Object);
 
-        ENCRYPT_KEY: string;
-        SIGN_KEY: string;
-        INIT_VECTOR: string;
-
-        encrypt(plaintext: string): Buffer;
-
-        decrypt(ciphertext: string): Buffer;
-
-        sign(message: string): Buffer;
-
-        verify(signature: Buffer, message: string): ShimCrypto.VerifyResponse;
-
-    }
-
-    export interface VerifyResponse {
-        ok: boolean;
-        error?: Error;
-    }
-}
